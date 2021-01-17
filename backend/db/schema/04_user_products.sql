80bc75b4a32cf0a5bf220f973576d43723432d78
Samantha Fok <samantha.fok@hotmail.com>
2021-01-17 23:23:38 +0000

    Created schema for user_products

---
+++ backend/db/schema/04_user_products.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS user_products CASCADE;
+
+CREATE TABLE user_products (
+   id SERIAL PRIMARY KEY NOT NULL,
+  product_title VARCHAR(255) NOT NULL,
+  goal INT NOT NULL,
+  amount_reached INT NOT NULL,
+  description TEXT NOT NULL,
+  category_id INT REFERENCES categories(id) on DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  image VARCHAR(255) NOT NULL,
+  accomplished BOOLEAN NOT NULL,
+  creator_profile_id INT REFERENCES creator_profile(id) on DELETE CASCADE,
+  number_of_donations INT NOT NULL
+)
+
+
+
