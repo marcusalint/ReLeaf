8c0a204c48ef3e4f27cfccb858578444557b31d3
Samantha Fok <samantha.fok@hotmail.com>
2021-01-17 23:24:30 +0000

    Created sample seeds for user_products

---
+++ backend/db/seeds/04_user_products.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_products(product_title, goal, amount_reached, description, category_id, image, accomplished, creator_profile_id, number_of_donations)
+VALUES
+     ('Hygiene products for my family', 500, 0, 'This donation will provide my family with toothpaste, soap, and feminine products.', 1, 'https://assets.website-files.com/5d9ba0eb5f6edb77992a99d0/5e625cf2d5f36f929c2a517b_5741139-512.png', false, 1, 0),
+     ('Medical products for my family', 1000, 200, 'donation will provide my family with medicines and other supplies', 2,'https://www.wework.com/ideas/wp-content/uploads/sites/4/2018/05/Medinas-HERO-1120x630.jpg', false,2,20),
+     ('Clothing, we lost it all', 700, 20, 'This donation will help recover the clothes we lost in the disaster', 3, 'https://www.flaticon.com/svg/vstatic/svg/2934/2934972.svg?token=exp=1610926200~hmac=1ec7551ba30ff6198016c40e308a3705', false, 1, 1)
+
