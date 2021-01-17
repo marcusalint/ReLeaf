dc187e8466ab89dd081a054ceb3aaad026785489
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-17 20:55:25 +0000

    routes file content changed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
+
+// Taken from Midterm - Sam
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+    .then(data => {
+        console.log("We are in the Users query!")
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
