9bb18ac1da1baf9b7320dfad34cb841f99a3f074
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2021-01-18 13:23:56 -0800

    Created AllFundsList component, no features yet

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 export default function Home(props) {
   return(
     <div>
       <h2>This is the Home Page here</h2>
+      <Button 
+      variant="contained" 
+      color="primary" 
+      href="#contained-buttons">
+        Create a Fund
+      </Button>
+      <Button 
+      variant="contained" 
+      color="primary" 
+      href="#contained-buttons">
+        Contribute to a Fund
+      </Button>
     </div>
   )
 }
