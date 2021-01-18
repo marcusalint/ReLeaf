0d95b1fc6a2b57e58fbff10c574f66734a745493
Samantha Fok <samantha.fok@hotmail.com>
2021-01-18 02:58:13 +0000

    Created a Home component and made sure it rendered in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Home from './components/Home';
 
+export default function App(props) {
+  return (
+    <div className="App" >
+      <h1> Releaf </h1>
+      <Home/>
+    </div >
+  );
+}
+
+// const useApplicationData = require("./hooks/useApplicationData")
+// import useApplicationData from "./hooks/useApplicationData"
 // function App() {
 //   return (
 //     <div className="App">
@@ -21,20 +34,20 @@ import './App.css';
 //     </div>
 //   );
 // }
-
-const App = () => {
-  const {
-      state,
-      dispatch
-  } = useApplicationData();
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
-)   );
-  return (<div className="App" >
-    <h1> Users </h1>
-    <ul> {userList} </ul>
-  </div >
-  );
-};
-
-
-export default App;
+// const App = () => {
+// //   const {
+// //       state,
+// //   } = useApplicationData;
+// //     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+// // )   );
+//   return (
+//     <div className="App" >
+//       <h1> Releaf </h1>
+//       <Home/>
+//     </div >
+//   );
+//   // return (
+//   //   <h1>Hello</h1>
+//   // )
+// };
+// export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home(props) {
+  return(
+    <div>
+      <h2>This is the Home Page here</h2>
+    </div>
+  )
+}
