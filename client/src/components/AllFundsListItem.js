aa3bfd67b485c53cadcc0958f8266da164581d90
Samantha Fok <samantha.fok@hotmail.com>
2021-01-19 04:09:53 +0000

    Created AllFundsListItem component that receives props from AllFundsList and renders creator_profile

--- client/src/components/AllFundsListItem.js
+++ client/src/components/AllFundsListItem.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import "./AllFundsListItem.css";
 
 export default function AllFundsListItem(props) {
   return (
+    // <li>
+    //   {props.name}
+    // </li>
     <li className="fund" key={props.id} >
       <img className="fund__img" 
       src={props.image} 
@@ -10,7 +13,7 @@ export default function AllFundsListItem(props) {
       <p>{props.location}</p>
       <h4>{props.title}</h4>
       <p>{props.description}</p>
-      <span>{props.amount_raised}raise of {props.total_goal}</span>
+      <span>{props.amount_raised} raise of {props.total_goal}</span>
     </li>
   );
 }
