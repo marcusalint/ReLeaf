4dfc1adc954b687569e6f0955eb19bc9b5066f59
Samantha Fok <samantha.fok@hotmail.com>
2021-01-19 22:47:27 +0000

    Changed Login to Logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -66,7 +66,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Releaf
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Logout</Button>
         </Toolbar>
       </AppBar>
     </div>
