70a46bae559d29d5d77270c87946966d435e10bb
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-20 02:35:48 +0000

    added get and post for creatorProfile

---
+++ backend/routes/creatorProfile.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router  = express.Router();
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+   
+    db.query(`SELECT * FROM creator_profile;`)
+    .then(data => {
+        const creatorProfile = data.rows;
+        res.json({ creatorProfile });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  router.post("/",(req, res) => {
+    const value = req.body.title;
+     //console.log(value);
+    const queryParams = [req.body.state.title,req.body.state.description, req.body.state.image, req.body.state.total_goal,1];
+    const queryString = "insert into creator_profile(title,description,image,total_goal,user_id) values($1,$2,$3,$4,$5)";
+
+    return db.query(queryString,queryParams)
+    .then(data => {
+      res.status(200).json({result: true})
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+
+  })
+  return router;
+};
