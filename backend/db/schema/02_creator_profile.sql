ca663af5c2a8147a92f357286f5e643cc64b1d40
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-20 02:34:22 +0000

    posts.sql schema deleted and renamed its seeds file

---
+++ backend/db/schema/02_creator_profile.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS creator_profile CASCADE;
+
+CREATE TABLE creator_profile(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    amount_raised INT DEFAULT 0,
+    total_goal INT NOT NULL,
+    total_goal_reached BOOLEAN DEFAULT false,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+    
