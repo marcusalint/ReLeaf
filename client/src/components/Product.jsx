6705a5286edfbde6de709e9ec66adc4584909e1f
Samantha Fok <samantha.fok@hotmail.com>
2021-01-21 07:48:25 +0000

    Modified Typography attributes to fix bug

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -18,14 +18,14 @@ const Product = ({product}) => {
         <CardMedia className={classes.media} image='' title={product.product_title}/>
         <CardContent>
           <div className={classes.cardcontent}>
-            <Typography variant="h8" gutterBottom>
+            <Typography>
               {Product.product_title}
             </Typography>
-            <Typography variant="h8">
+            <Typography>
               {Product.product_title}
             </Typography>
           </div>
-          <Typography variant="h8" color="testSecondary"> 
+          <Typography> 
           {product.description}
           </Typography>
           {/* <Typography variant="h8" color="testSecondary"> 
@@ -39,7 +39,7 @@ const Product = ({product}) => {
         </CardActions>
       </Card>
       <StripeCheckout 
-      stripekey="pk_test_51IBuSOAj9EPpC5TEcXDX4CGoDapFJkSGFryFE06LaZOWzsBf9BBjJU22dAAmcswiJLFrNNdU9aGw2od6hfqNrkD5004yMieTFP"
+      stripekey="pk_test_51I6hkfKOSKL0JejWWdM5Q7z0rI12q1tnP06rUIjlLFIAqciOHrwaDAwIxkHSgVrhRld9fUXrm8Hf3Fxbd3qJ4Syq002ujDH8Il"
       token={handleToken}
       />
     </div>
