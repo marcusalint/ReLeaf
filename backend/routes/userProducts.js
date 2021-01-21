1f9431fc5e612cbe83a678f426cf2de86627635d
Samantha Fok <samantha.fok@hotmail.com>
2021-01-21 07:47:50 +0000

    Added missing route endpoint

--- backend/routes/userProducts.js
+++ backend/routes/userProducts.js
@@ -2,9 +2,30 @@ const express = require('express');
 const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM user_products WHERE user_id = 1;`)
+    console.log("Making the get request")
+    db.query(`SELECT * FROM user_products`)
     .then(data => {
+        console.log("We are in the Users Products query!")
+        console.log(data, "this is the data that we are accessing") 
         const user_products = data.rows;
+        console.log(data.rows, 'this is the data from data.rows')
+        res.json({ user_products });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id, 'This is the req params console log')
+    db.query(`SELECT * FROM user_products WHERE user_id = ${id};`)
+    .then(data => {
+        console.log("We are in the Users Products query!")
+        console.log(data, "this is the data that we are accessing") 
+        const user_products = data.rows;
+        console.log(data.rows, 'this is the data from data.rows')
         res.json({ user_products });
       })
       .catch(err => {
@@ -14,4 +35,4 @@ module.exports = (db) => {
       });
   });
   return router;
-};
+}; 
