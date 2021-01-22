b54958b6b9c310d545b1f4a1a61c882b3c06cc10
Samantha Fok <samantha.fok@hotmail.com>
2021-01-22 11:50:31 +0000

    Made 'Releaf' link to /home

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -33,6 +33,7 @@
 
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -40,7 +41,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import './Navbar.css'
+import './Navbar.scss'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -66,7 +67,9 @@ export default function ButtonAppBar() {
           <img className="logo" src="/images/plant.png"/>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Releaf
+            <Link to={'/home'} style={{ textDecoration: 'none' }} className="link-home">
+              Releaf
+            </Link>
           </Typography>
           <Button color="inherit">Logout</Button>
         </Toolbar>
