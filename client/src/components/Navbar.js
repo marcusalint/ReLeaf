0164e63b39cdc5bdcc106ba3b55a5ce8b1212336
salar <salar.taeb@hotmail.com>
2021-01-23 08:03:05 +0000

    debugging navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -40,7 +40,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import './Navbar.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -59,11 +58,10 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" color="default">
+      <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            {/* <MenuIcon /> */}
-          <img className="logo" src="/images/plant.png"/>
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Releaf
