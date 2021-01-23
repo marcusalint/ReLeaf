4d506cdf264fdeed8f9e864142202a76f49c872f
salar <salar.taeb@hotmail.com>
2021-01-23 08:00:58 +0000

    adding new routes for userProducts

--- backend/routes/userProducts.js
+++ backend/routes/userProducts.js
@@ -1,14 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// module.exports = router;
-
-// Taken from Midterm - Sam
 const express = require('express');
 const router  = express.Router();
 
@@ -17,10 +6,9 @@ module.exports = (db) => {
     console.log("Making the get request")
     db.query(`SELECT * FROM user_products`)
     .then(data => {
-        console.log("We are in the Users Products query!")
-        console.log(data, "this is the data that we are accessing") 
+      
         const user_products = data.rows;
-        console.log(data.rows, 'this is the data from data.rows')
+   
         res.json({ user_products });
       })
       .catch(err => {
@@ -31,13 +19,12 @@ module.exports = (db) => {
   });
   router.get("/:id", (req, res) => {
     const id = req.params.id;
-    console.log(id, 'This is the req params console log')
+   
     db.query(`SELECT * FROM user_products WHERE user_id = ${id};`)
     .then(data => {
-        console.log("We are in the Users Products query!")
-        console.log(data, "this is the data that we are accessing") 
+      
         const user_products = data.rows;
-        console.log(data.rows, 'this is the data from data.rows')
+      
         res.json({ user_products });
       })
       .catch(err => {
@@ -46,5 +33,27 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+  router.post("/", (req, res) => {
+    const goal = req.body.productObj.goal;
+    const donations_needed = req.body.productObj.donations_needed;
+    const price_of_donation = goal/donations_needed;
+    const amount_reached = req.body.productObj.amount_reached;
+    const id = req.body.productObj.id;
+    const number_of_donations = req.body.productObj.number_of_donations;
+    
+    db.query(`UPDATE user_products SET amount_reached = ${amount_reached + price_of_donation}, number_of_donations = ${number_of_donations + 10} WHERE id = ${id}`)
+    .then(data => {
+       
+        const user_products = data.rows;
+      
+        res.json({ user_products });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });   
   return router;
 }; 
+
