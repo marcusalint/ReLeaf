d6da559103c9f101d0b10279b15ff50efb5f7bae
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-24 07:11:50 +0000

    got changes from git pull

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,38 +1,64 @@
-import React from 'react';
+import React, { useState }from 'react';
 import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from "@material-ui/core";
 import { AddShoppingCart} from "@material-ui/icons";
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios'
 import {toast} from 'react-toastify'
-
 import 'react-toastify/dist/ReactToastify.css'
 // import "./styles.css"
-
 import useStyles from './styles'
-
 toast.configure();
 
 const Product = ({product}) => {
+  const [product1, setProduct] = useState(
+    {
+      amount_reached: product.amount_reached
+    }
+  )
+  console.log(product1)
+  let price_per_donation = (product.goal/product.donations_needed)
+  console.log(price_per_donation)
   const classes = useStyles();
-
   async function handleToken(token, addresses) {
     console.log(token, addresses)
    const response = await axios.post("http://localhost:3000/api/checkout", {
       token,
       product
+    }, function() {
+      console.log(token, 'I can access the token material here')
     })
-    const {status} = response.data
-    console.log(response.data)
+    const {status, price} = response.data
+    // console.log(response.data, 'this is the response data')
+    // console.log(price, 'this is the price')
     if (status === 'success') {
       toast('Success! Check emails for details', 
       {type: 'success'})
       console.log('transaction was successful')
+      // console.log(response)
+      let a = JSON.parse(response.config.data)
+      let productObj = a.product;
+      // console.log(a.token.id, 'this is a token')
+      console.log(a.product, 'this is the product')
+      axios.post("http://localhost:3000/api/userProducts", { productObj })
+      .then(res => {
+        const amount = product1.amount_reached;
+        product1.amount_reached = amount + price_per_donation;
+        console.log(res.config.data, "This is the UPDATED value to set state")
+        setProduct((prev) => ({...prev, amount_reached: product1.amount_reached}) )
+        console.log()
+      })
+      // if (a) {
+      //   this.setState({
+      //     token.id : a.token.id
+      //     // this class has your current state of your product object and we're setting product.product token id to the token id we just 
+      //   })
+      // }
     } else {
       toast('Something went wrong', 
       {type: 'error'})
     }
   }
-
+  console.log(product1, "PRODUCT1 STATE")
   return (
     <div className="container">
       <Card className={classes.root}>
@@ -49,8 +75,8 @@ const Product = ({product}) => {
           <Typography > 
           </Typography>
           <Typography> 
-          {product.goal}
-          {console.log(product.goal)}
+          {price_per_donation}
+          {console.log(price_per_donation)}
           </Typography>
           {/* <Typography variant="h8" color="testSecondary"> 
           {product}
@@ -65,20 +91,23 @@ const Product = ({product}) => {
       <StripeCheckout 
       stripeKey="pk_test_51IBuSOAj9EPpC5TEcXDX4CGoDapFJkSGFryFE06LaZOWzsBf9BBjJU22dAAmcswiJLFrNNdU9aGw2od6hfqNrkD5004yMieTFP"
       token={handleToken}
-      amount={product.goal*100}
+      amount={price_per_donation*100}
       />
+      <h1>THIS IS A TEST OF WHETHER THIS WILL RENDER ON THE SCREEN{product1.amount_reached}</h1>
     </div>
-
-    // <div>
-    //   <h1>{Product.product_title}</h1>
-    //   <h1>{Product.goal}</h1>
-    //   <h1>{Product.description}</h1>
-    //   <h1>{Product.amount_reached}</h1>
-    //   <h1>{Product.user_id}</h1>
-    //   <h1>{Product.accomplished}</h1>
-    //   <h1>{Product.number_of_donations}</h1>
-    // </div>
   )
 }
-
 export default Product
+
+
+
+
+
+
+
+
+
+
+
+
+
