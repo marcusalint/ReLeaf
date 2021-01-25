80f53928dec1bc23b84a6d29b24461d96a1b9ed9
Samantha Fok <samantha.fok@hotmail.com>
2021-01-25 19:21:36 +0000

    Created a GET route to contributions

--- backend/routes/contributions.js
+++ backend/routes/contributions.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router  = express.Router();
-
 module.exports = (db) => {
   router.get("/", (req, res) => {
     console.log('we hitting the contributions table')
+    db.query(`SELECT * FROM contributions`)
+    .then(data => {
+      const contributions = data.rows
+      res.json({ contributions })
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({error: err.message})
+    })
+  })
+  router.post("/", (req, res) => {
+    console.log('post route successfully working')
   })
   return router;
 };
-
-
