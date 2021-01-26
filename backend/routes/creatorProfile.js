4cace706293768ea3eb715f1ea46815e9ad3e2c7
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-26 01:07:43 +0000

    added new get route to /:id

--- backend/routes/creatorProfile.js
+++ backend/routes/creatorProfile.js
@@ -15,7 +15,22 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-
+  router.get("/:id", (req, res) => {
+    console.log("CREATOR PROFILE ROUTE")
+    const id = req.params.id;
+    console.log(id,"This is the id");
+    db.query(`SELECT * FROM creator_profile WHERE user_id = ${id};`)
+    .then(data => {
+      console.log(data.data);
+        const posts = data.rows;
+        res.json({ posts });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
   // Updates The Total Amount of Money Raised 
   router.post("/", (req, res) => {
     console.log(req.body, 'this is the response back after hitting creatorProfile111')
@@ -26,7 +41,7 @@ module.exports = (db) => {
     // Destructuring Assignment For ProductObj 
     const {id, product_title, goal, amount_reached, description, category_id, user_id, created_at, image, accomplished, creator_profile_id, number_of_donations, donations_needed} = productObj;
 
-    const price_of_donation = goal/donations_needed;
+    const price_of_donation = goal/10;
 
      db.query(`SELECT amount_raised FROM creator_profile WHERE user_id = ${user_id}`)
       .then(data => {
@@ -52,21 +67,7 @@ module.exports = (db) => {
       });
   });
   
-  router.post("/",(req, res) => {
-    const value = req.body.title;
-     //console.log(value);
-    const queryParams = [req.body.state.title,req.body.state.description, req.body.state.image, req.body.state.total_goal,1];
-    const queryString = "insert into creator_profile(title,description,image,total_goal,user_id) values($1,$2,$3,$4,$5)";
-    return db.query(queryString,queryParams)
-    .then(data => {
-      res.status(200).json({result: true})
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-  })
+ 
   return router;
 };
 
