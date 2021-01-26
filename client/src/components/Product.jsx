bc2414895b93a1c5c5ac7819300e1b4d4c14764f
Samantha Fok <samantha.fok@hotmail.com>
2021-01-26 01:17:16 +0000

    Removed axios.get to creatorProfile because no longer needed and also removed console logs

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,27 +1,30 @@
-import React, { useState }from 'react';
+import React, { useState, useEffect }from 'react';
 import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from "@material-ui/core";
 import { AddShoppingCart} from "@material-ui/icons";
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios'
-import {toast} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios';
+import {toast} from 'react-toastify';
+import ProgressBar from './progressbar/index.js';
+import 'react-toastify/dist/ReactToastify.css';
+import "./Product.scss";
 // import "./styles.css"
 import useStyles from './styles' 
 toast.configure();
 
+
 const Product = ({product}) => {
   const [product1, setProduct] = useState(
     {
-      amount_reached: product.amount_reached
+      amount_reached: product.amount_reached,
+      donations_needed: product.donations_needed
     }
   )
-  console.log(product1)
-  let price_per_donation = (product.goal/product.donations_needed)
-  console.log(price_per_donation)
+
   const classes = useStyles();
+
+  const price_per_donation = (product.goal/10)
   async function handleToken(token, addresses) {
     console.log(token, addresses)
-
    // Process Payment 
    const response = await axios.post("http://localhost:3000/api/checkout", {
       token,
@@ -30,77 +33,72 @@ const Product = ({product}) => {
       console.log(token, 'I can access the token material here')
     })
     const {status, price} = response.data
-    console.log(response.data, 'this is the response data')
-    console.log(price, 'this is the price')
-
+  
     // Check To See If the Payment Went Through
     if (status === 'success') {
       toast('Success! Check emails for details',
       {type: 'success'})
-      console.log('transaction was successful')
-    // console.log(response)
-      let a = JSON.parse(response.config.data)
-      let productObj = a.product;
-      console.log(a.token.id, 'this is a token')
-      console.log(a.product, 'this is the product')
-
-      
-
+      const a = JSON.parse(response.config.data)
+      const productObj = a.product;
+      // Posts to contributions table
+      // axios.post("http://localhost:3000/api/contributions", {productObj})
+      // .then(res => {
+      //   console.log(res, 'posting to contributions')
+      // }) 
       // Update user_products Table After Payment Success
       axios.post("http://localhost:3000/api/userProducts", { productObj })
       .then(res => {
-        const amount = product1.amount_reached;
-        product1.amount_reached = amount + price_per_donation;
-        console.log(res.config.data, "This is the UPDATED value to set state")
-        setProduct((prev) => ({...prev, amount_reached: product1.amount_reached}) )
-        console.log()
+        const amount_reached = product1.amount_reached + price_per_donation;
+        const donations_needed = product1.donations_needed - 1; 
+        setProduct((prev) => ({...prev, amount_reached, donations_needed}) )
       })
-      
       // Update creator_profile Table
       axios.post("http://localhost:3000/api/creatorProfile" , {productObj})
       .then(res => {
-        console.log(productObj, 'yeah man we are getting the right response line 47')
       })
-      
-      // 
-      axios.post("http://localhost:3000/api/userProducts/updateTimestamp", {productObj})
-      .then(res => {
-        console.log('update timestamp route works so far')
-      }) 
-
     } else {
       toast('Something went wrong',
       {type: 'error'})
     }
   }
-  console.log(product1, "PRODUCT1 STATE")
+  const getPercentage = function(props) {
+    const percent = (product1.amount_reached/product.goal)*100;
+    return percent;
+  }
   return (
-    <div className="container">
+    <div className="product--container">
       <Card className={classes.root}>
         <CardMedia className={classes.media} image='' title={product.product_title}/>
         <CardContent>
-          <div className={classes.cardcontent}>
+          <div className="product--text--container">
+            <img src={product.image} alt="" className="product--image"/>
+            <div className={classes.cardcontent} id="product--text">
+              <Typography className="product--title">
+                <strong>{product.product_title}</strong>
+              </Typography>
+              <Typography className="product--description">
+                {product.description}
+              </Typography>
+            </div>
+            
             <Typography>
-              {product.product_title}
-            </Typography>
-            <Typography >
-              {product.product_title}
+              <span>{product1.donations_needed} x <strong className="text--bold">${price_per_donation}</strong></span>
             </Typography>
           </div>
-          <Typography >
-          </Typography>
+
           <Typography>
-          {price_per_donation}
-          {console.log(price_per_donation)}
+            <div className="product--progress">
+              <ProgressBar percentage={getPercentage()}/>
+              <span className="fund--goal">
+                <strong>${product1.amount_reached.toLocaleString()} raised</strong> of ${product.goal.toLocaleString()} goal
+              </span>
+            </div>
           </Typography>
-          {/* <Typography variant="h8" color="testSecondary">
-          {product}
-          </Typography> */}
         </CardContent>
         <CardActions disableSpacing className={classes.cardActions}>
-          <IconButton aria-label="Add to Cart">
+          {/* <IconButton aria-label="Add to Cart">
             <AddShoppingCart />
-          </IconButton>
+          </IconButton> */}
         </CardActions>
       </Card>
       <StripeCheckout
@@ -108,21 +106,7 @@ const Product = ({product}) => {
       token={handleToken}
       amount={price_per_donation*100}
       />
-      <h1>THIS IS A TEST OF WHETHER THIS WILL RENDER ON THE SCREEN{product1.amount_reached}</h1>
     </div>
   )
 }
 export default Product
-
-
-
-
-
-
-
-
-
-
-
-
-
