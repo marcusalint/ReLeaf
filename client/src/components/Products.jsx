3e5632c87f741c223ceab2f065e108c923098316
Samantha Fok <samantha.fok@hotmail.com>
2021-01-26 00:59:52 +0000

    Passing the props of a particular user

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,33 +1,13 @@
-
-import {React, useEffect, useState} from 'react';
+import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product'
-import axios from 'axios';
-
-// const products = [
-//   { id: 1, name: 'Shoes', description: 'Running shoes', price:"$5"},
-//   { id: 2, name: 'Macbook', description: 'Apple macbook', price:"$10"},
-//   { id: 3, name: 'Clothes', description: 'Help ', price:"$10"}
-// ];
-
-
-
-
-
-const Products = () => {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-  axios.get('http://localhost:3000/api/userProducts/1')
-  .then((data) => {
-    setProducts(data.data.user_products)
-  })
-  },[]);
+//Pass in props 
+const Products = (props) => {
 
   return (
     <main>
     <Grid container justify="center" spacing={4}> 
-        {products.map((product) => {
+        {props.products.map((product) => {
           return (
             <Grid item key={product.id}>
             <Product product={product}/>
