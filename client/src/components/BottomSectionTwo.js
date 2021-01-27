8d2a75a959ffecf585ef45da763361533ff9a7a5
Salar <salar.taeb@hotmail.com>
2021-01-27 01:58:30 -0800

    Adding Campaign Pag bottom right component

---
+++ client/src/components/BottomSectionTwo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./BottomSectionTwo.scss";
+
+
+const BottomSectionTwo = () => {
+  return (
+    <div className="Bottom--Right--Container">
+      <div className="Right--Panel">
+        <h1>dsadasddadasdad</h1>
+      </div>
+    </div>
+  )
+}
+
+export default BottomSectionTwo
