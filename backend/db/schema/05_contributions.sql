3a88935081f624aff17bd70bfbfd7242e4d2a8a5
Samantha Fok <samantha.fok@hotmail.com>
2021-01-28 04:54:14 +0000

    Added timestamp to contributions

--- backend/db/schema/05_contributions.sql
+++ backend/db/schema/05_contributions.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS contributions CASCADE;
 CREATE TABLE contributions(
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INT REFERENCES users(id) on DELETE CASCADE,
-  user_products_id INT REFERENCES user_products(id) on DELETE CASCADE
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_products_id INT REFERENCES user_products(id) on DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
