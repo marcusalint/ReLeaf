9b256b85056e00be4c6d8e35c1e6a67da637299c
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-28 05:46:39 +0000

    updated structure of list

--- client/src/components/FormList.js
+++ client/src/components/FormList.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+import './Form.css';
 
 export default function FormList(props) {
+  const Hygiene = "Hygiene";
+  const Medical = "Medical";
+  const Clothing = "Clothing";
+  const Funeral = "Funeral";
+  const Housing = "Housing"
   return (
-    <tr className="list-item">
-        <td>{props.categories}</td>
-        <td>{props.price}</td>
-        <td>{props.product_title}</td>
-        <td>{props.description}</td>
-    </tr>
+    <ul className="list-item">
+        <li className="list-categories" >{props.categories} - </li>
+        <li className="list-categories"><strong>Price</strong> : {props.price}$</li>
+        <li className="list-categories"><strong>Title</strong> : {props.product_title}</li>
+        <li className="list-categories"><strong>Description</strong> : {props.description}</li>
+    </ul>
   );
 };
