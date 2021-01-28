f8549ef51e61a83bcd64babd456447eb8ed6abe3
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-28 06:23:15 +0000

    removed category li tag

--- client/src/components/FormList.js
+++ client/src/components/FormList.js
@@ -2,17 +2,13 @@ import React from "react";
 import './Form.css';
 
 export default function FormList(props) {
-  const Hygiene = "Hygiene";
-  const Medical = "Medical";
-  const Clothing = "Clothing";
-  const Funeral = "Funeral";
-  const Housing = "Housing"
+ 
   return (
     <ul className="list-item">
-        <li className="list-categories" >{props.categories} - </li>
-        <li className="list-categories"><strong>Price</strong> : {props.price}$</li>
-        <li className="list-categories"><strong>Title</strong> : {props.product_title}</li>
-        <li className="list-categories"><strong>Description</strong> : {props.description}</li>
+       
+        <li className="list-categories"><strong>Price : </strong>{props.price}$</li>
+        <li className="list-categories"><strong>Title : </strong> {props.product_title}</li>
+        <li className="list-categories"><strong>Description : </strong> {props.description}</li>
     </ul>
   );
 };
