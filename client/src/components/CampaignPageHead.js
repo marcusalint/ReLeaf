b8112af82db2fa77c8ac92e7cec7da83a5943904
salar <salar.taeb@hotmail.com>
2021-01-28 07:46:17 +0000

    rearranging components, updating functions to reflect state

--- client/src/components/CampaignPageHead.js
+++ client/src/components/CampaignPageHead.js
@@ -3,14 +3,15 @@ import './CampaignPageHead.scss'
 import HeadSectionOne from './HeadSectionOne'
 import HeadSectionTwo from './HeadSectionTwo'
 
+
 const CampaignPageHead = ({
-  products, updateProduct, users, profile, goal
+  products, updateProduct, users, profile, goal, state
 }) => {
 
   return (
     <div className='Head--Banner'>
-      <HeadSectionOne products={products} updateProduct={updateProduct} users={users} profile={profile} goal={goal}/>
-      <HeadSectionTwo products={products} updateProduct={updateProduct} users={users} profile={profile} goal={goal}/>
+      <HeadSectionOne products={products} updateProduct={updateProduct} users={users} profile={profile} goal={goal}state={state}/>
+      <HeadSectionTwo products={products} updateProduct={updateProduct} users={users} profile={profile} goal={goal} state={state}/>
     </div>
   )
 }
