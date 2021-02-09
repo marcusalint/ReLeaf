11cff6369e231f1c8935af829080adc39d9b9954
Samantha Fok <samantha.fok@hotmail.com>
2021-02-09 05:28:21 +0000

    Added a placeholder for the image of a creator_profile to be shown on the campaign page. Must updated products table for it to be fully implemented

--- client/src/components/HeadSectionOne.js
+++ client/src/components/HeadSectionOne.js
@@ -14,6 +14,8 @@ const HeadSectionOne = ({products, updateProduct, users, profile, goal, state}) 
   return (
     <div className="Section--One">
       <img src="https://images.unsplash.com/photo-1567742940521-26d7040e1fd6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80" className="Head--Image"></img>
+
+      {/* <img src={users[0].image} className="Head--Image"></img> THIS WORKS BUT MUST ADD PRODUCTS TO CREATOR_PROFILES 4, 5, AND 6 FOR THEM TO RENDER*/}
   <h5 className="User--Location">{users[0].location}</h5>
     <div className="Icons">
   <button className="Share--Button">Share</button>
